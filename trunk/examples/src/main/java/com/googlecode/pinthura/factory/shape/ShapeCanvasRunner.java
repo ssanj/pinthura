29d8cee83f9bbd24769c7c5b3d837f555d6ce55e
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2008-10-06 11:32:15 +0000

    1. Added Shape example.

---
+++ trunk/examples/src/main/java/com/googlecode/pinthura/factory/shape/ShapeCanvasRunner.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.pinthura.factory.shape;
+
+import com.googlecode.pinthura.factory.FactoryCreator;
+import com.googlecode.pinthura.factory.FactoryCreatorImpl;
+import com.googlecode.pinthura.factory.InvocationFactoryImpl;
+
+public final class ShapeCanvasRunner {
+
+    public static void main(final String[] args) {
+        FactoryCreator factoryCreator = new FactoryCreatorImpl(new InvocationFactoryImpl());
+        SquareFactory squareFactory = factoryCreator.create(SquareFactory.class);
+
+        ShapeCanvas canvas = new ShapeCanvas(40, squareFactory);
+        System.out.println("Number of squares: " + canvas.calcSquaresOnCanvas(2));
+    }
+}
