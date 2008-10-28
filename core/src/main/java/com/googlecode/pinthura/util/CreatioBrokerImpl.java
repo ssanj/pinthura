a5adb601d61792d887e2b1d380e907ffff8f98b0
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2008-10-28 11:08:59 +0000

    1. Initial implementation of CreationBroker.

---
+++ core/src/main/java/com/googlecode/pinthura/util/CreatioBrokerImpl.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.pinthura.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class CreatioBrokerImpl implements CreationBroker {
+
+    private final Map<Class<?>, CreationListener> listeners = new HashMap<Class<?>, CreationListener>();
+
+    public  <T> void addCreationListener(final Class<T> clazzOfInterest, final CreationListener<T> creationListener) {
+        listeners.put(clazzOfInterest, creationListener);
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public <T> void notifyInstanceCreated(final T instance) {
+        for (Class<?> clazz : listeners.keySet()) {
+            if (clazz.isAssignableFrom(instance.getClass())) {
+                //TODO: Just return the 1st one.
+                listeners.get(clazz).instanceCreated(instance);
+            }
+        }
+    }
+
+    //TODO: make this thread safe.
+}
