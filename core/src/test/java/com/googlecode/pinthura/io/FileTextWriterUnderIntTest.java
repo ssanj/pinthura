c3448e9a0c2be29e55292a7aa21ae348b7e1e809
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2008-12-04 13:35:21 +0000

    1. Added integration test for FileTextWriter.

---
+++ core/src/test/java/com/googlecode/pinthura/io/FileTextWriterUnderIntTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.pinthura.io;
+
+import com.googlecode.pinthura.io.boundary.WriterBoundary;
+import com.googlecode.pinthura.io.boundary.WriterBoundaryImpl;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.StringWriter;
+import java.util.Arrays;
+
+public final class FileTextWriterUnderIntTest {
+
+    private FileTextWriter writer;
+    private StringWriterFactory stringWriterFactory;
+
+    @Before
+    public void setup() {
+        stringWriterFactory = new StringWriterFactory();
+        writer = new FileTextWriterImpl(stringWriterFactory);
+    }
+
+    @Test
+    public void shouldWriteSuppliedSources() {
+        writer.write("", Arrays.asList("one", " two ", "three"));
+        assertThat(stringWriterFactory.getWriter().toString(), equalTo("one two three"));
+    }
+
+    private static class StringWriterFactory implements FileWriterFactory {
+
+        private StringWriter writer;
+
+        public WriterBoundary create(final String fileName, final boolean append) {
+            writer = new StringWriter();
+            return new WriterBoundaryImpl(writer);
+        }
+
+        public StringWriter getWriter() {
+            return writer;
+        }
+    }
+}
