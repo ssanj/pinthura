0187e6c95b1631fb6a6519f79f9af3d5ba546cf8
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2008-12-06 06:50:25 +0000

    1. Added exception tests for FileTextWriter.

---
+++ branches/Release-0.2.0.0/core/src/test/java/com/googlecode/pinthura/io/FileTextWriterThrowingExceptionsUnderTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.pinthura.io;
+
+import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public final class FileTextWriterThrowingExceptionsUnderTest {
+
+    private final IMocksControl mockControl = EasyMock.createControl();
+    private FileTextWriter writer;
+    private FileWriterFactory mockFileWriterFactory;
+
+    @Before
+    public void setup() {
+        mockFileWriterFactory = mockControl.createMock(FileWriterFactory.class);
+        writer = new FileTextWriterImpl(mockFileWriterFactory);
+    }
+
+    @SuppressWarnings({ "ThrowableInstanceNeverThrown" })
+    @Test(expected = FileTextWriterException.class)
+    public void shouldThrowAnExceptionIfIfFileWriterFactoryThrowsAnException() {
+        String fileName = "foo.bar.fib";
+        EasyMock.expect(mockFileWriterFactory.create(fileName, false)).andThrow(new RuntimeException());
+        mockControl.replay();
+
+        writer.write(fileName, Arrays.asList("testing987"));
+    }
+
+    @Test(expected = FileTextWriterException.class)
+    public void shouldThrownAnExceptionIfFileWriterFactoryReturnsNull() {
+        String fileName = "foo.bar.fib";
+        EasyMock.expect(mockFileWriterFactory.create(fileName, false)).andReturn(null);
+        mockControl.replay();
+
+        writer.write(fileName, Arrays.asList("testing987"));
+    }
+}
