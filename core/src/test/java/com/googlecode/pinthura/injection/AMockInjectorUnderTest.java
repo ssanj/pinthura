84dd0b5a075b7cfc889f8a5ebaf32b054ded33c2
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2009-01-17 06:14:19 +0000

    1. TDDing MockInjector.

---
+++ core/src/test/java/com/googlecode/pinthura/injection/AMockInjectorUnderTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.pinthura.injection;
+
+import com.googlecode.pinthura.annotation.SuppressionReason;
+import com.googlecode.pinthura.injection.data.RandomIntegralValueIncubator;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class AMockInjectorUnderTest {
+
+    @SuppressWarnings({"InstanceVariableOfConcreteClass"})
+    @SuppressionReason(SuppressionReason.Reason.INCUBATOR)
+    private RandomIntegralValueIncubator incubator;
+
+    @Before
+    public void setup() {
+        incubator = new RandomIntegralValueIncubator();
+        new MockInjectorImpl().inject(incubator);
+    }
+
+    @Test
+    public void shouldInjectMocksIntoSuppliedInstance() {
+        incubator.createRandomIntegralValue()
+                 .supplyRandomSeed(0.5)
+                 .performGetRandomValue()
+                 .observeValue(100)
+                 .isReturned()
+                 .done();
+
+    }
+}
---
+++ core/src/test/java/com/googlecode/pinthura/injection/data/RandomIntegralValueIncubator.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.pinthura.injection.data;
+
+import com.googlecode.pinthura.annotation.SuppressionReason;
+import com.googlecode.pinthura.boundary.java.lang.MathBoundary;
+import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+@SuppressWarnings({"MethodReturnOfConcreteClass"})
+@SuppressionReason(SuppressionReason.Reason.INCUBATOR)
+public final class RandomIntegralValueIncubator {
+
+    @SuppressWarnings({"InstanceVariableOfConcreteClass"})
+    @SuppressionReason(SuppressionReason.Reason.TEST_VALUE)
+    private RandomIntegralValue cut;
+    private MathBoundary mockMathBoundary;
+    private IMocksControl mockControl;
+    private int returnedRandomValue;
+
+
+    public RandomIntegralValueIncubator createRandomIntegralValue() {
+        cut = new RandomIntegralValue(mockMathBoundary);
+        return this;
+    }
+
+    public RandomIntegralValueIncubator supplyRandomSeed(double seed) {
+        EasyMock.expect(mockMathBoundary.random()).andReturn(seed);
+        return this;
+    }
+
+
+    public RandomIntegralValueIncubator performGetRandomValue() {
+        mockControl.replay();
+
+        returnedRandomValue = cut.getRandomValue();
+        return this;
+    }
+
+    public RandomIntegralValueIncubator observeValue(int expectedValue) {
+        assertThat(returnedRandomValue, equalTo(expectedValue));
+        return this;
+    }
+
+    public RandomIntegralValueIncubator isReturned() {
+        return this;
+    }
+
+    public void done() {
+        mockControl.verify();
+    }
+
+    private static final class RandomIntegralValue {
+
+        private MathBoundary mathBoundary;
+
+        private RandomIntegralValue(final MathBoundary mathBoundary) {
+            this.mathBoundary = mathBoundary;
+        }
+
+
+        public int getRandomValue() {
+            return (int) (mathBoundary.random() * 200);
+        }
+    }
+
+}
