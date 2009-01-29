b45f0deb459a7815f61db7d5e171092ab016d872
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2009-01-29 03:40:10 +0000

    Renamed MockInject* -> EasyMock*

---
+++ core/src/main/java/com/googlecode/pinthura/injection/EasyMockInjectorBuilder.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.pinthura.injection;
+
+import com.googlecode.pinthura.annotation.SuppressionReason;
+import com.googlecode.pinthura.reflection.FieldFinder;
+import com.googlecode.pinthura.reflection.FieldFinderImpl;
+import com.googlecode.pinthura.reflection.FieldSetter;
+import com.googlecode.pinthura.reflection.FieldSetterImpl;
+
+@SuppressWarnings({"MethodReturnOfConcreteClass"})
+@SuppressionReason(SuppressionReason.Reason.BUILDER_PATTERN)
+public final class EasyMockInjectorBuilder {
+
+    private FieldFinder fieldFinder;
+    private FieldSetter fieldSetter;
+    private EasyMockWrapper easyMockWrapper;
+    private MockConfigurer mockConfigurer;
+
+    public EasyMockInjectorBuilder() {
+        fieldFinder = new FieldFinderImpl();
+        fieldSetter = new FieldSetterImpl();
+        easyMockWrapper = new EasyMockWrapperImpl();
+        mockConfigurer = new MockPrefixMockConfigurer();
+    }
+
+    public EasyMockInjectorBuilder withFieldFinder(final FieldFinder fieldFinder) {
+        this.fieldFinder = fieldFinder;
+        return this;
+    }
+
+    public EasyMockInjectorBuilder withFieldSetter(final FieldSetter fieldSetter) {
+        this.fieldSetter = fieldSetter;
+        return this;
+    }
+
+    public EasyMockInjectorBuilder withEasyMockWrapper(final EasyMockWrapper easyMockWrapper) {
+        this.easyMockWrapper = easyMockWrapper;
+        return this;
+    }
+
+    public EasyMockInjectorBuilder withMockConfigurer(final MockConfigurer mockConfigurer) {
+        this.mockConfigurer = mockConfigurer;
+        return this;
+    }
+
+    public MockInjector build() {
+        return new EasyMockInjectorImpl(mockConfigurer, fieldFinder, fieldSetter, easyMockWrapper);
+    }
+}
