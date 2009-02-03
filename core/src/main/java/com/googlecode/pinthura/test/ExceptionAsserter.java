24b9200a071417a9a1e88ea9286c71f57d9f1f5e
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2009-02-03 22:48:17 +0000

    1.Refactored ExceptionAsserter.

--- core/src/main/java/com/googlecode/pinthura/test/ExceptionAsserter.java
+++ core/src/main/java/com/googlecode/pinthura/test/ExceptionAsserter.java
@@ -6,15 +6,15 @@ public interface ExceptionAsserter {
 
     <EX> void assertValidException(final Throwable exception, final Class<EX> exceptionClass);
 
-    <EX, NEX> void assertException(final Class<EX> expectedExceptionClass, final Class<NEX> expectedNestedExceptionClass,
+    <EX, NEX> void runAndAssertException(final Class<EX> expectedExceptionClass, final Class<NEX> expectedNestedExceptionClass,
                                                  final String message, final ExceptionAsserterImpl.Exceptional ex);
 
-    <EX, NEX> void assertException(final Class<EX> expectedExceptionClass, final Class<NEX> expectedNestedExceptionClass,
+    <EX, NEX> void runAndAssertException(final Class<EX> expectedExceptionClass, final Class<NEX> expectedNestedExceptionClass,
                                                  final ExceptionAsserterImpl.Exceptional ex);
 
-    <EX> void assertException(final Class<EX> expectedExceptionClass, final ExceptionAsserterImpl.Exceptional ex);
+    <EX> void runAndAssertException(final Class<EX> expectedExceptionClass, final ExceptionAsserterImpl.Exceptional ex);
 
-    <EX> void assertException(final Class<EX> expectedExceptionClass, final String message,
+    <EX> void runAndAssertException(final Class<EX> expectedExceptionClass, final String message,
                                             final ExceptionAsserterImpl.Exceptional ex);
 
     interface Exceptional {
