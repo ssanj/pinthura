21dfb65f62510da55b4a0d997315f0e19504f770
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2009-02-06 05:29:14 +0000

    1.Underconstruction - fixing tests.

--- core/src/test/java/com/googlecode/pinthura/factory/instantiator/injected/ADynamicFactoryInstantiatorUnderTest.java
+++ core/src/test/java/com/googlecode/pinthura/factory/instantiator/injected/ADynamicFactoryInstantiatorUnderTest.java
@@ -15,65 +15,92 @@
  */
 package com.googlecode.pinthura.factory.instantiator.injected;
 
+import com.googlecode.pinthura.annotation.SuppressionReason;
+import com.googlecode.pinthura.boundary.java.lang.ClassBoundary;
+import com.googlecode.pinthura.boundary.java.lang.reflect.ConstructorBoundary;
+import com.googlecode.pinthura.data.Shape;
+import com.googlecode.pinthura.data.UrlBoundary;
+import com.googlecode.pinthura.factory.MethodParam;
+import com.googlecode.pinthura.factory.instantiator.AnnotatedClassExtractor;
+import com.googlecode.pinthura.factory.instantiator.InstantiationStrategy;
+import com.googlecode.pinthura.filter.MatchNotFoundException;
+import com.googlecode.pinthura.test.ExceptionAsserter;
+import com.googlecode.pinthura.test.Exceptional;
+import com.googlecode.pinthura.test.ExceptionAsserterImpl;
+import com.googlecode.pinthura.util.Arrayz;
+import com.googlecode.pinthura.util.ArrayzImpl;
+import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsSame.sameInstance;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
 public final class ADynamicFactoryInstantiatorUnderTest {
 
-//    private final IMocksControl mockControl = EasyMock.createControl();
-//    private AnnotatedClassExtractor mockAnnotatedClassExtractor;
-//    private InjectedFactoryResolver mockInjectedFactoryResolver;
-//    private InstantiationStrategy instantiator;
-//    private MethodParam mockMethodParam;
-//
-//    @Before
-//    public void setup() {
-//        mockAnnotatedClassExtractor = mockControl.createMock(AnnotatedClassExtractor.class);
-//        mockInjectedFactoryResolver = mockControl.createMock(InjectedFactoryResolver.class);
-//        mockMethodParam = mockControl.createMock(MethodParam.class);
-//
-//        instantiator = new DynamicFactoryInstantiator(mockAnnotatedClassExtractor, mockInjectedFactoryResolver);
-//    }
-//
-//    @SuppressWarnings({ "unchecked" })
-//    @Test
-//    public void shouldCreateAnInstanceOfTheRequestedFactory() {
-//        ClassBoundary mockImplementation = mockControl.createMock(ClassBoundary.class);
-//        ClassBoundary mockConstructorArg = mockControl.createMock(ClassBoundary.class);
-//        UrlBoundary mockUrlBoundary = mockControl.createMock(UrlBoundary.class);
-//        Shape mockShape = mockControl.createMock(Shape.class);
-//        InjectedFactoryValues mockInjectedFactoryValues = mockControl.createMock(InjectedFactoryValues.class);
-//        EasyMock.expect(mockAnnotatedClassExtractor.extract(mockMethodParam)).andReturn(mockImplementation);
-//        EasyMock.expect(mockInjectedFactoryResolver.resolve(mockMethodParam)).andReturn(mockInjectedFactoryValues);
-//
-//        ClassBoundary[] constructorTypes = ArrayzImpl.fromObjects(mockConstructorArg);
-//        Object[] arguments = ArrayzImpl.fromObjects(mockUrlBoundary);
-//        EasyMock.expect(mockInjectedFactoryValues.getConstructorTypes()).andReturn(constructorTypes);
-//        EasyMock.expect(mockInjectedFactoryValues.getConstructorArguments()).andReturn(arguments);
-//        ConstructorBoundary mockConstructorBoundary = mockControl.createMock(ConstructorBoundary.class);
-//        EasyMock.expect(mockImplementation.getConstructor(constructorTypes)).andReturn(mockConstructorBoundary);
-//        EasyMock.expect(mockConstructorBoundary.newInstance(arguments)).andReturn(mockShape);
-//        mockControl.replay();
-//
-//        Shape result = (Shape) instantiator.filter(mockMethodParam);
-//        assertThat(result, sameInstance(mockShape));
-//
-//        mockControl.verify();
-//    }
-//
-//    @Test
-//    public void shouldReturnItsName() {
-//        assertThat("Dynamic Parameter Instantiator", equalTo(instantiator.getFilterName()));
-//    }
-//
-//    @SuppressWarnings({ "unchecked", "ThrowableInstanceNeverThrown" })
-//    @Test
-//    public void shouldThrowAMatchNotFoundExceptionIfAnExceptionIsThrown() {
-//        EasyMock.expect(mockAnnotatedClassExtractor.extract(mockMethodParam)).andThrow(new IllegalArgumentException());
-//        mockControl.replay();
-//
-//        try {
-//            instantiator.filter(mockMethodParam);
-//        } catch (MatchNotFoundException e) {
-//            mockControl.verify();
-//            assertThat((Class<IllegalArgumentException>) e.getCause().getClass(), equalTo(IllegalArgumentException.class));
-//        }
-//    }
+    private final IMocksControl mockControl = EasyMock.createControl();
+    private AnnotatedClassExtractor mockAnnotatedClassExtractor;
+    private InjectedFactoryResolver mockInjectedFactoryResolver;
+    private InstantiationStrategy instantiator;
+    private MethodParam mockMethodParam;
+    private Arrayz arrayz;
+    private ExceptionAsserter exceptionAsserter;
+
+    @Before
+    public void setup() {
+        mockAnnotatedClassExtractor = mockControl.createMock(AnnotatedClassExtractor.class);
+        mockInjectedFactoryResolver = mockControl.createMock(InjectedFactoryResolver.class);
+        mockMethodParam = mockControl.createMock(MethodParam.class);
+        arrayz = new ArrayzImpl();
+        exceptionAsserter = new ExceptionAsserterImpl();
+
+        instantiator = new DynamicFactoryInstantiator(mockAnnotatedClassExtractor, mockInjectedFactoryResolver);
+    }
+
+    @SuppressWarnings("unchecked")
+    @SuppressionReason(SuppressionReason.Reason.CANT_INFER_GENERICS_ON_MOCKS)
+    @Test
+    public void shouldCreateAnInstanceOfTheRequestedFactory() {
+        ClassBoundary mockImplementation = mockControl.createMock(ClassBoundary.class);
+        ClassBoundary mockConstructorArg = mockControl.createMock(ClassBoundary.class);
+        UrlBoundary mockUrlBoundary = mockControl.createMock(UrlBoundary.class);
+        Shape mockShape = mockControl.createMock(Shape.class);
+        InjectedFactoryValues mockInjectedFactoryValues = mockControl.createMock(InjectedFactoryValues.class);
+        EasyMock.expect(mockAnnotatedClassExtractor.extract(mockMethodParam)).andReturn(mockImplementation);
+        EasyMock.expect(mockInjectedFactoryResolver.resolve(mockMethodParam)).andReturn(mockInjectedFactoryValues);
+
+        ClassBoundary[] constructorTypes = arrayz.fromObjects(mockConstructorArg);
+        Object[] arguments = arrayz.fromObjects(mockUrlBoundary);
+        EasyMock.expect(mockInjectedFactoryValues.getConstructorTypes()).andReturn(constructorTypes);
+        EasyMock.expect(mockInjectedFactoryValues.getConstructorArguments()).andReturn(arguments);
+        ConstructorBoundary mockConstructorBoundary = mockControl.createMock(ConstructorBoundary.class);
+        EasyMock.expect(mockImplementation.getConstructor(constructorTypes)).andReturn(mockConstructorBoundary);
+        EasyMock.expect(mockConstructorBoundary.newInstance(arguments)).andReturn(mockShape);
+        mockControl.replay();
+
+        Shape result = (Shape) instantiator.filter(mockMethodParam);
+        assertThat(result, sameInstance(mockShape));
+
+        mockControl.verify();
+    }
+
+    @Test
+    public void shouldReturnItsName() {
+        assertThat("Dynamic Parameter Instantiator", equalTo(instantiator.getFilterName()));
+    }
+
+    @SuppressWarnings({"ThrowableInstanceNeverThrown"})
+    @SuppressionReason(SuppressionReason.Reason.TEST_VALUE)
+    @Test
+    public void shouldThrowAMatchNotFoundExceptionIfAnExceptionIsThrown() {
+        EasyMock.expect(mockAnnotatedClassExtractor.extract(mockMethodParam)).andThrow(new IllegalArgumentException("boom"));
+        mockControl.replay();
+
+        exceptionAsserter.runAndAssertException(MatchNotFoundException.class, IllegalArgumentException.class, "boom",
+                new Exceptional() {@Override public void run() { instantiator.filter(mockMethodParam); }
+        });
+
+        mockControl.verify();
+    }
 }
