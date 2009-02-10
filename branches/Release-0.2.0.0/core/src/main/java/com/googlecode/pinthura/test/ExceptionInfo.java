27c174bc43a49f6df3989aaba7b50721d9e11c09
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2009-02-10 22:15:40 +0000

    1. Introduced ExceptionInfo to encapsulate exceptions.

--- branches/Release-0.2.0.0/core/src/main/java/com/googlecode/pinthura/test/ExceptionInfo.java
+++ branches/Release-0.2.0.0/core/src/main/java/com/googlecode/pinthura/test/ExceptionInfo.java
@@ -11,6 +11,11 @@ public interface ExceptionInfo {
     String NO_MESSAGE = "EXCEPTIONINFO -> NO MESSAGE";
 
     /**
+     * Implementation of <code>NullExceptionInfo</code> used to signify the lack of an exception.
+     */
+    NullExceptionInfo NO_EXCEPTION = new NullExceptionInfo();
+
+    /**
      * Returns the exception <code>Class</code>.
      * @return The exception <code>Class</code>.
      */
@@ -27,4 +32,48 @@ public interface ExceptionInfo {
      * @return The nested exception.
      */
     ExceptionInfo getNestedException();
+
+    /**
+     * Return true if <code>ExceptionInfo</code> has a nested exception, false if not.
+     * @return True if <code>ExceptionInfo</code> has a nested exception, false if not.
+     */
+    boolean hasNestedException();
+
+    /**
+     * Return true if <code>ExceptionInfo</code> has a message, false if not.
+     * @return True if <code>ExceptionInfo</code> has a message, false if not.
+     */
+    boolean hasMessage();
+
+    /**
+     * Models a null object representing the state where an exception has not been supplied. All methods throw an
+     * <code>UnsupportedOperationException</code>.
+     */
+    class NullExceptionInfo implements ExceptionInfo {
+
+        @Override
+        public Class<? extends Throwable> getExceptionClass() {
+            throw new UnsupportedOperationException("getExceptionClass() called on NullExceptionInfo.");
+        }
+
+        @Override
+        public String getMessage() {
+            throw new UnsupportedOperationException("getMessage() called on NullExceptionInfo.");
+        }
+
+        @Override
+        public ExceptionInfo getNestedException() {
+            throw new UnsupportedOperationException("getNestedException() called on NullExceptionInfo.");
+        }
+
+        @Override
+        public boolean hasNestedException() {
+            throw new UnsupportedOperationException("hasNestedException() called on NullExceptionInfo.");
+        }
+
+        @Override
+        public boolean hasMessage() {
+            throw new UnsupportedOperationException("hasMessage() called on NullExceptionInfo.");
+        }
+    }
 }
