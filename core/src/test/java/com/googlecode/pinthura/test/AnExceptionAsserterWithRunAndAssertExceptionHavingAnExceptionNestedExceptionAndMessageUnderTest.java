557d896df29408e18869d39bf58de86d7e20a6c4
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2009-02-12 21:56:38 +0000

    1. Using ExceptionMessageBuilderForClass for class messages.

--- core/src/test/java/com/googlecode/pinthura/test/AnExceptionAsserterWithRunAndAssertExceptionHavingAnExceptionNestedExceptionAndMessageUnderTest.java
+++ core/src/test/java/com/googlecode/pinthura/test/AnExceptionAsserterWithRunAndAssertExceptionHavingAnExceptionNestedExceptionAndMessageUnderTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.googlecode.pinthura.test;
 
 import com.googlecode.pinthura.annotation.SuppressionReason;
@@ -53,7 +68,7 @@ public final class AnExceptionAsserterWithRunAndAssertExceptionHavingAnException
         } catch (AssertionError e) {
             assertThat(e.getMessage(), equalTo(
                     exceptionMessageBuilder.withExpectedClass(ArrayIndexOutOfBoundsException.class).
-                                            withReceivedClass(PathEvaluatorException.class).build()));
+                                            andReceivedClass(PathEvaluatorException.class).build()));
 
         }
     }
@@ -72,7 +87,7 @@ public final class AnExceptionAsserterWithRunAndAssertExceptionHavingAnException
         } catch (AssertionError e) {
             assertThat(e.getMessage(),
                     equalTo(exceptionMessageBuilder.withExpectedClass(NullPointerException.class).
-                                                    withReceivedClass(FileNotFoundException.class).build()));
+                                                    andReceivedClass(FileNotFoundException.class).build()));
         }
     }
 
--- core/src/test/java/com/googlecode/pinthura/test/ExceptionMessageBuilder.java
+++ core/src/test/java/com/googlecode/pinthura/test/ExceptionMessageBuilder.java
@@ -1,19 +1,28 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.googlecode.pinthura.test;
 
 public final class ExceptionMessageBuilder {
-    private Class<?> expectedClass;
-    private Class<?> receivedClass;
     private Object expectedObject;
     private Object receivedObject;
+    private ExceptionMessageBuilderForClass classBuilder;
 
-    public ExceptionMessageBuilder withExpectedClass(final Class<?> expectedClass) {
-        this.expectedClass = expectedClass;
-        return this;
-    }
-
-    public ExceptionMessageBuilder withReceivedClass(final Class<?> receivedClass) {
-        this.receivedClass = receivedClass;
-        return this;
+    public ExceptionMessageBuilderForClass withExpectedClass(final Class<?> expectedClass) {
+        classBuilder = new ExceptionMessageBuilderForClass(this, expectedClass);
+        return classBuilder;
     }
 
     public ExceptionMessageBuilder withExpectedObject(final Object expectedObject) {
@@ -27,10 +36,8 @@ public final class ExceptionMessageBuilder {
     }
 
     public String build() {
-        if (expectedClass != null && receivedClass != null) {
-            return new StringBuilder().
-                    append("\nExpected: <").append(expectedClass).append(">\n     ").
-                    append("got: <").append(receivedClass).append(">\n").toString();
+        if (classBuilder != null) {
+            return classBuilder.build();
         }
 
         if (expectedObject != null && receivedObject != null) {
@@ -40,5 +47,29 @@ public final class ExceptionMessageBuilder {
         }
 
         return "";
+    }
+
+    public static final class ExceptionMessageBuilderForClass {
+
+        private final ExceptionMessageBuilder parent;
+        private Class<?> receivedClass;
+        private final Class<?> expectedClass;
+
+        private ExceptionMessageBuilderForClass(final ExceptionMessageBuilder parent, final Class<?> expectedClass) {
+            this.parent = parent;
+            this.expectedClass = expectedClass;
+        }
+
+        ExceptionMessageBuilder andReceivedClass(final Class<?> receivedClass) {
+            this.receivedClass = receivedClass;
+            return parent;
+        }
+
+        String build() {
+            return new StringBuilder().
+                    append("\nExpected: <").append(expectedClass).append(">\n     ").
+                    append("got: <").append(receivedClass).append(">\n").toString();
+
+        }
     }
 }
