2312a0e65e788ff18135a82988e85617ca2a5f61
sanjivsahayam <sanjivsahayam@cac35766-8702-11dd-aace-ddc5e87cd236>
2009-02-17 07:22:08 +0000

    1. javadoc.

--- core/src/main/java/com/googlecode/pinthura/util/ArrayzImpl.java
+++ core/src/main/java/com/googlecode/pinthura/util/ArrayzImpl.java
@@ -22,41 +22,17 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-/**
- * A collection of useful methods for working with arrays.
- */
 public final class ArrayzImpl implements Arrayz {
 
-    /**
-     * Given a variable number of objects of a type <T> returns an T[] of the same objects.
-     * @param objects The objects to include in the T[].
-     * @param <T> The type of objects in the array.
-     * @return A T[] of the supplied objects.
-     */
     public <T> T[] fromObjects(final T... objects) {
         return objects;
     }
 
-    /**
-     * Given a <code>Collection</code> of elements of type <T> returns its contents as a T[].
-     * @param collection The <code>Collection</code> whose items are to be returned as a T[].
-     * @param clazz The Class<T>
-     * @param <T> The type of object in the <code>Collection</code>.
-     * @return A T[] built from the contents of the <code>Collection</code> supplied. If the a <code>Set<T></code> is supplied the order of
-     * elements returned in the T[] is indeterminate.
-     */
     @SuppressWarnings("unchecked")
     public <T> T[] fromCollection(final Collection<? extends T> collection, final Class<T> clazz) {
         return collection.toArray((T[]) Array.newInstance(clazz, collection.size()));
     }
 
-    /**
-     * Filters the items in a supplied <code>Collection</code> of type <T> and returns the result in a <code>List</code> of the same type.
-     * @param items The items to be filtered.
-     * @param itemFilter The filter to use when filtering items.
-     * @param <T> The type of items.
-     * @return A <code>List<T></code> without the items filtered out by the supplied filter.
-     */
     public <T> List<T> filter(final Collection<T> items, final ItemFilter<T> itemFilter) {
         List<T> filteredList = new ArrayList<T>();
 
